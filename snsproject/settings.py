fa96ade41439c17f01d698f0cd14d4d9e0ef9aa4
umamichikun <ratherbe99@gmail.com>
2021-04-28 11:47:29 +0300

    Django sns app, first commit

--- snsproject/settings.py
+++ snsproject/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from . import newsapi
+#from . import newsapi
+from newsapi import NewsApiClient
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
